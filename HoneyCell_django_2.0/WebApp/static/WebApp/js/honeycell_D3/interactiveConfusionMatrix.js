2e5eac7894b3f4373a0c64b617bf54819f2d545f
luojianhe1992 <luojianhe1992@gmail.com>
2016-04-20 12:03:16 -0400

    Fix the bug about confusion matrix.

--- HoneyCell_django_2.0/WebApp/static/WebApp/js/honeycell_D3/interactiveConfusionMatrix.js
+++ HoneyCell_django_2.0/WebApp/static/WebApp/js/honeycell_D3/interactiveConfusionMatrix.js
@@ -4,44 +4,44 @@
 
 
 
-var margin = {top: 20, right: 20, bottom: 30, left: 80},
-    width = 700 - margin.left - margin.right,
-    height = 350 - margin.top - margin.bottom;
-
-var formatPercent = d3.format(".0%");
-
-var x = d3.scale.ordinal()
-    .rangeRoundBands([0, width], .1, 1);
-
-var y = d3.scale.linear()
-    .range([height, 0]);
-
-var xAxis = d3.svg.axis()
-    .scale(x)
-    .orient("bottom");
-
-var yAxis = d3.svg.axis()
-    .scale(y)
-    .orient("left")
-    .tickFormat(formatPercent);
-
-var tip = d3.tip()
-    .attr('class', 'd3-tip')
-    .direction('n')
-    .offset([-10, 2])
-    .html(function (d) {
-        return "<strong>Precision:</strong> <span style='color:red'>" +
-            d.Precision + "</span>";
-    });
-
-var svg = d3.select("#confusion_matrix")
-    .append("svg")
-    .attr("width", width + margin.left + margin.right)
-    .attr("height", height + margin.top + margin.bottom)
-    .append("g")
-    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-
-svg.call(tip);
+//var margin = {top: 20, right: 20, bottom: 30, left: 80},
+//    width = 700 - margin.left - margin.right,
+//    height = 350 - margin.top - margin.bottom;
+//
+//var formatPercent = d3.format(".0%");
+//
+//var x = d3.scale.ordinal()
+//    .rangeRoundBands([0, width], .1, 1);
+//
+//var y = d3.scale.linear()
+//    .range([height, 0]);
+//
+//var xAxis = d3.svg.axis()
+//    .scale(x)
+//    .orient("bottom");
+//
+//var yAxis = d3.svg.axis()
+//    .scale(y)
+//    .orient("left")
+//    .tickFormat(formatPercent);
+//
+//var tip = d3.tip()
+//    .attr('class', 'd3-tip')
+//    .direction('n')
+//    .offset([-10, 2])
+//    .html(function (d) {
+//        return "<strong>Precision:</strong> <span style='color:red'>" +
+//            d.Precision + "</span>";
+//    });
+//
+//var svg = d3.select("#confusion_matrix")
+//    .append("svg")
+//    .attr("width", width + margin.left + margin.right)
+//    .attr("height", height + margin.top + margin.bottom)
+//    .append("g")
+//    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+//
+//svg.call(tip);
 
 
 var task_id = document.getElementById("task_id").value;
@@ -69,7 +69,7 @@ d3.json("/get_json_result/" + task_id, function(error, json_data){
 
 
     // call the dashboard function
-    dashboard('#dashboard', freqData);
+    dashboard('#confusion_matrix', freqData);
 
 
     function dashboard(id, fData) {
